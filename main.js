c30f26598b21833ba90d95dd05d9a76b4e34d717
teak1 <trainian@gmail.com>
2017-02-21 14:23:07 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
-var first = document.querySelector('#number1');
-var second = document.querySelector('#number2');
-
-var result = document.querySelector('.result');
-
 if (window.Worker) { // Check if Browser supports the Worker api.
-	// Requires script name as input
-	var myWorker = new Worker("worker.js");
-
-// onkeyup could be used instead of onchange if you wanted to update the answer every time
-// an entered value is changed, and you don't want to have to unfocus the field to update its .value
-
-	first.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]); // Sending message as an array to the worker
-	  console.log('Message posted to worker');
-	  console.log([first.value,second.value]);
-	};
-
-	second.onchange = function() {
-	  myWorker.postMessage([first.value,second.value]);
-	  console.log('Message posted to worker');
-	  console.log([first.value,second.value]);
-	};
-
-	myWorker.onmessage = function(e) {
-		result.textContent = e.data;
-		console.log('Message received from worker');
-		console.log(e);
-	};
+	window.worker=function(url,recieve){
+		this.worker = new Worker(url||"");
+		this.worker.onmessage = function(e){
+			this.recieve(e.data);
+		}
+		this.recieve = recieve;
+	}
+	window.worker.prototype=Object.assign(Worker.prototype,{
+		send(data){
+			this.worker.postMessage(data);
+		},
+	});
 }
