3fb26fa78e440c50a520216c1a0664f6545296ed
teak1 <trainian@gmail.com>
2017-02-21 14:36:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+var messages = {};
+function rp(){
+  var o = "";
+  for(var i = 0;i<20;i++){
+    o+=String.fromCharCode(Math.round(Math.random()*2200)+34);
+  }
+  if(Object.keys(messages).includes(o))return rp();
+  messages[o]=null;
+  return o;
+}
 if (window.Worker) { // Check if Browser supports the Worker api.
 	window.worker=function(url,recieve){
+    this.recievePos = rp();
 		this.worker = new Worker(url||"");
 		this.worker.onmessage = function(e){
-			this.recieve(e.data);
+			messages[this.recievePos](e.data);
 		}
-		this.worker.recieve = recieve;
+		messages[this.recievePos] = recieve;
 	}
 	window.worker.prototype=Object.assign(Worker.prototype,{
 		send(data){
